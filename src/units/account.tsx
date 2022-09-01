436233479be998b509669968198bd34880d00772
Rachel Ralston <rachel.ralston@kofile.com>
2022-09-01 17:08:29 -0400

    feat(account): page content

--- src/units/account.tsx
+++ src/units/account.tsx
@@ -1,3 +1,21 @@
-const Account = () => <h1>Account Page</h1>;
+import { useAuth0 } from "@auth0/auth0-react";
+import Button from "../components/button";
+
+const Account = () => {
+  const { user, logout } = useAuth0();
+
+  return (
+    <div>
+      <h1>Account Page</h1>
+      <Button text="logout" onClick={logout} />
+      {user !== undefined && (
+        <>
+          <p>email: {user.email}</p>
+          <img src={user.picture} />
+        </>
+      )}
+    </div>
+  );
+};
 
 export default Account;
