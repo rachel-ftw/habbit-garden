86f4835b3f51d470ae450f790c9921c271ba260d
Rachel Ralston <rachel.ralston@kofile.com>
2022-09-06 13:43:16 -0400

    chore: generate mock plants

--- src/utils/mocks-plants.ts
+++ src/utils/mocks-plants.ts
@@ -1,50 +1,64 @@
 import { v4 as uuidv4 } from "uuid";
+import getRandomInt from "./get-random-int";
+
+const trees = [
+  { name: "ash", cost: 1200 },
+  { name: "willow", cost: 457 },
+  { name: "maple", cost: 500 },
+  { name: "hardwood", cost: 457 },
+  { name: "pine", cost: 567 },
+];
+
+const flowers = [
+  { name: "daisy", cost: 50 },
+  { name: "rose", cost: 200 },
+  { name: "mugwort", cost: 35 },
+  { name: "camomile", cost: 39 },
+  { name: "lavender", cost: 79 },
+];
+
+const shrubs = [
+  { name: "pine", cost: 100 },
+  { name: "hibiscus", cost: 200 },
+  { name: "holly", cost: 300 },
+  { name: "blackberry", cost: 400 },
+  { name: "grape", cost: 500 },
+];
+
+const generatePlant = ({ name, cost }: any) => ({
+  id: uuidv4(),
+  name,
+  cost,
+  image: "some shit",
+});
+
+enum PlantTypes {
+  shrubs,
+  flowers,
+  trees,
+}
+
+export const generatePlants = (type: PlantTypes) => {
+  const mocks = {
+    [PlantTypes.shrubs]: shrubs,
+    [PlantTypes.flowers]: flowers,
+    [PlantTypes.trees]: trees,
+  };
+
+  const availablePlants = mocks[type];
+  const makeNum = () => getRandomInt(availablePlants.length) || 1;
+
+  return Array(makeNum())
+    .fill("r")
+    .map((_, index) => generatePlant(availablePlants[makeNum()]));
+};
 
 const plants = {
   units: 200,
-  purchased: {
-    tree: [
-      {
-        id: uuidv4(),
-        name: "ash",
-        image: "/tree/#tree-id",
-        cost: 100,
-      },
-    ],
-    flower: [
-      {
-        id: uuidv4(),
-        name: "daisy",
-        image: "/flower/#flower-id",
-        cost: 100,
-      },
-      {
-        id: uuidv4(),
-        name: "rose",
-        image: "/flower/#flower-id",
-        cost: 100,
-      },
-      {
-        id: uuidv4(),
-        name: "mugwort",
-        image: "/flower/#flower-id",
-        cost: 100,
-      },
-    ],
-    shrub: [
-      {
-        id: uuidv4(),
-        name: "hibiscus",
-        image: "/flower/#flower-id",
-        cost: 100,
-      },
-      {
-        id: uuidv4(),
-        name: "pine",
-        image: "/flower/#flower-id",
-        cost: 100,
-      },
-    ],
+  available: {
+    tree: generatePlants(PlantTypes.trees),
+    flower: generatePlants(PlantTypes.flowers),
+    shrub: generatePlants(PlantTypes.shrubs),
   },
 };
 
