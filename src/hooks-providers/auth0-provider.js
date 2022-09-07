57838e96cd1197c18e7b3a5b57a1b9e3eddb5454
Rachel Ralston <rachel.ralston@kofile.com>
2022-09-07 11:57:22 -0400

    chore(env): init netlify

--- src/hooks-providers/auth0-provider.js
+++ src/hooks-providers/auth0-provider.js
@@ -2,20 +2,23 @@ import { Auth0Provider } from "@auth0/auth0-react";
 import { useNavigate } from "react-router-dom";
 
 const Auth0ProviderWithNavigation = ({ children }) => {
+  const navigate = useNavigate();
+  const onRedirectCallback = () => navigate("/game");
+
   const domain = process.env.REACT_APP_AUTH0_DOMAIN;
   const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
+  const env = process.env.REACT_APP_ENV;
+  const netlifyUrl = process.env.REACT_APP_NETLIFY_URL;
+  const localHostUrl = "http://localhost:3000/";
 
-  const navigate = useNavigate();
-
-  const onRedirectCallback = () => {
-    navigate("/game");
-  };
+  const redirectUri = env === "development" ? localHostUrl : netlifyUrl;
+  console.log({ netlifyUrl, redirectUri, env });
 
   return (
     <Auth0Provider
       domain={domain}
       clientId={clientId}
-      redirectUri="http://localhost:3000/"
+      redirectUri={redirectUri}
       onRedirectCallback={onRedirectCallback}
     >
       {children}
